cd67a69bce086e498fe3379c6c4ecc330a037d0a
Ibrahim Mansour <mansourbrahim0@gmail.com>
2024-01-22 11:04:27 -0800

    Big O NOTATION

--- sort.h
+++ sort.h
@@ -1,6 +1,5 @@
-#ifndef SORT_H
-#define SORT_H
-
+#ifndef _SORT_H_
+#define _SORT_H_
 #include <stddef.h>
 
 /**
@@ -23,4 +22,15 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+
 #endif
